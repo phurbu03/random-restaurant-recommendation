73da56db98828e20820e06b42ec1bbd83ede8d66
phurbu03 <phurbu03@naver.com>
2024-11-24 18:15:13 +0900

    1

--- server.js
+++ server.js
@@ -2,35 +2,30 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-
-const PORT = 3000;
+const PORT = 3000; // 로컬 서버 포트
 
 // 프록시 설정
-app.use(
-  '/api/places',
-  createProxyMiddleware({
-    target: 'https://maps.googleapis.com', // Google API의 기본 URL
-    changeOrigin: true, // CORS 해결
-    pathRewrite: {
-      '^/api/places': '/maps/api/place/nearbysearch/json', // 프록시 경로 재설정
-    },
-    onProxyReq: (proxyReq, req) => {
-      // API 키와 요청 파라미터를 쿼리에 추가
-      const params = new URLSearchParams({
-        location: '37.5495647,126.839655',
-        radius: '5000',
-        keyword: '한식',
-        type: 'restaurant',
-        opennow: 'true',
-        key: 'AIzaSyAgM5OqN72VeDmuTb9hMGGf2HxdZDGkREs', // Google Places API 키
-      });
-
-      proxyReq.path += `?${params.toString()}`;
-    },
-  })
+app.use('/api/places',
+    createProxyMiddleware({
+        target: 'https://maps.googleapis.com',
+        changeOrigin: true, // CORS 문제 해결
+        pathRewrite: { '^/api/places': '/maps/api/place/nearbysearch/json' },
+        onProxyReq: (proxyReq, req) => {
+            // API 요청에 필요한 매개변수 추가
+            const params = new URLSearchParams({
+                location: req.query.location,
+                radius: req.query.radius,
+                type: req.query.type,
+                opennow: req.query.opennow,
+                keyword: req.query.keyword,
+                key: 'AIzaSyAgM50qN72VeDmuTbh9MGcFzHXdZDGkREs' // Google Places API 키
+            });
+            proxyReq.path += `?${params.toString()}`;
+        },
+    })
 );
 
 // 서버 실행
 app.listen(PORT, () => {
-  console.log(`Proxy server running on http://localhost:${PORT}`);
+    console.log(`Proxy server running on http://localhost:${PORT}`);
 });
