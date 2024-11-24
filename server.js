b23769ee66e28cbdddd5567bcf043c697f31b410
rachel22kr <haseongyeon796@gmail.com>
2024-11-25 00:45:39 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const PORT = 3000; // 로컬 서버 포트
+const PORT = 3000; 
 
-// 프록시 설정
+
 app.use('/api/places',
     createProxyMiddleware({
         target: 'https://maps.googleapis.com',
         changeOrigin: true, // CORS 문제 해결
         pathRewrite: { '^/api/places': '/maps/api/place/nearbysearch/json' },
         onProxyReq: (proxyReq, req) => {
-            // API 요청에 필요한 매개변수 추가
+            
             const params = new URLSearchParams({
                 location: req.query.location,
                 radius: req.query.radius,
